884bc3ef290b091282934e6b7b37d0bf6a0fd015
Vincenius <vincent.will@vaitrade.de>
2023-06-05 10:57:06 +0200

    add month to calendar

--- components/Calendar/Calendar.js
+++ components/Calendar/Calendar.js
@@ -11,49 +11,70 @@ const Calendar = ({ user = {}, variant = "small" }) => {
     : size.width >= 520 ? 20: 10;
   const allWorkouts = user.workouts || []
 
-  return <Flex justify="center">
-    {variant !== 'small' && <Flex direction="column" mr="sm">
-      <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Mo</Text>
-      <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Tu</Text>
-      <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>We</Text>
-      <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Th</Text>
-      <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Fr</Text>
-      <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Sa</Text>
-      <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Su</Text>
+  return <>
+    {variant !== 'small' && <Flex justify="center">
+      <Checkbox
+        mr={3}
+        className={styles.future}
+        readOnly
+      />
+      { [...Array(columns)].map((x, i) => {
+        const date = new Date();
+        date.setDate(date.getDate() + (7+(7-date.getDay())) % 7)
+        const days = (7*i)
+        date.setDate(date.getDate() - days);
+        const month = date.toLocaleString('en-US', { month: 'short' })
+        date.setDate(date.getDate() - 7);
+
+        return <Text key={`month-${i}`} c="dimmed" mr={3} style={{ fontSize: '0.7em', width: '20px', overflow: 'visible' }}>
+          { date.getDate() <= 7 ? month : '' }
+        </Text>
+      }).reverse() }
     </Flex> }
-    { [...Array(columns)].map((x, i) => {
-      return <Flex key={`calendar-${variant}-${i}`} direction={variant === 'small' ? 'row' : 'column'}>
-        { [...Array(rows)].map((x, j) => {
-          const date = new Date();
-          if (variant !== "small") {
-            date.setDate(date.getDate() + (7+(7-date.getDay())) % 7) // make it next sunday
-          }
+    <Flex justify="center">
+      {variant !== 'small' && <Flex direction="column" mr="sm">
+        <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Mo</Text>
+        <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Tu</Text>
+        <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>We</Text>
+        <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Th</Text>
+        <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Fr</Text>
+        <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Sa</Text>
+        <Text c="dimmed" style={{ fontSize: '0.7em', lineHeight: '2em' }}>Su</Text>
+      </Flex> }
+      { [...Array(columns)].map((x, i) => {
+        return <Flex key={`calendar-${variant}-${i}`} direction={variant === 'small' ? 'row' : 'column'}>
+          { [...Array(rows)].map((x, j) => {
+            const date = new Date();
+            if (variant !== "small") {
+              date.setDate(date.getDate() + (7+(7-date.getDay())) % 7) // make it next sunday
+            }
 
-          const days = (7*i) + j
-          date.setDate(date.getDate() - days);
-          const dayWorkouts = allWorkouts.filter(w => w.created_at.slice(0, 10) === date.toISOString().slice(0, 10))
-          const checked = dayWorkouts.some(w => w.exercises.every(e => e.completed))
+            const days = (7*i) + j
+            date.setDate(date.getDate() - days);
+            const dayWorkouts = allWorkouts.filter(w => w.created_at.slice(0, 10) === date.toISOString().slice(0, 10))
+            const checked = dayWorkouts.some(w => w.exercises.every(e => e.completed))
 
-          const isToday = (new Date().toDateString() === date.toDateString())
-          const isFuture = (new Date() < date)
-          const className = isToday
-            ? styles.today
-            : isFuture ? styles.future : ''
+            const isToday = (new Date().toDateString() === date.toDateString())
+            const isFuture = (new Date() < date)
+            const className = isToday
+              ? styles.today
+              : isFuture ? styles.future : ''
 
-          return <Tooltip label={date.toDateString()} key={`calendar-${variant}-${j}-${i}`}>
-            <Checkbox
-              color="green"
-              checked={checked || false}
-              mr={3}
-              mb={3}
-              className={className}
-              readOnly
-            />
-          </Tooltip>
-        }).reverse()}
-      </Flex>
-    }).reverse() }
-  </Flex>
+            return <Tooltip label={date.toDateString()} key={`calendar-${variant}-${j}-${i}`}>
+              <Checkbox
+                color="green"
+                checked={checked || false}
+                mr={3}
+                mb={3}
+                className={className}
+                readOnly
+              />
+            </Tooltip>
+          }).reverse()}
+        </Flex>
+      }).reverse() }
+    </Flex>
+  </>
 }
 
 export default Calendar

050523fe211fee675fa42dc0e7b76f410e636d43
Mathias <mathiasnouzy@gmail.com>
2023-06-14 00:17:56 +0200

    style: drag n drop list

--- components/Exercises/Exercises.js
+++ components/Exercises/Exercises.js
@@ -26,18 +26,13 @@ import {
 } from "./utils";
 import SelectModal from "./SelectModal";
 import VideoIcon from "./VideoIcon";
-import {
-  DragDropContext,
-  Droppable,
-  Draggable,
-} from "react-beautiful-dnd";
-import { IconArrowsMoveVertical } from '@tabler/icons-react';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { IconArrowsMoveVertical } from "@tabler/icons-react";
 
 const fetcher = (query) =>
   fetch(`/api/exercises${query}`).then((res) => res.json());
 
 const Exercises = ({ equipment, muscles, workout, setWorkout }) => {
-    const [isDragging, setIsDragging] = useState(false);
   const defaultCount = Math.round(6 / muscles.length) || 1; // default around 6 exercises
   const sortedEquipments = equipment.sort().join(",");
   const sortedMuscles = muscles.sort().join(",");
@@ -106,7 +101,6 @@ const Exercises = ({ equipment, muscles, workout, setWorkout }) => {
   };
 
   const onDragEnd = (result) => {
-    setIsDragging(false);
     if (!result.destination) return;
 
     const sourceIndex = result.source.index;
@@ -125,10 +119,6 @@ const Exercises = ({ equipment, muscles, workout, setWorkout }) => {
     setWorkout(updatedList);
   };
 
-  const onDragStart = (props) => {
-    setIsDragging(true);
-  }
-
   return (
     <div>
       <Paper shadow="none" p="xs" bg="#f1f3f5">
@@ -143,10 +133,10 @@ const Exercises = ({ equipment, muscles, workout, setWorkout }) => {
           </a>
         </Text>
       </Paper>
-      <Flex justify="space-between" mt="xl">
+      <Flex justify="space-between" mt="xl" direction="column">
         {(isLoading || !workout.length) && (
           <Timeline bulletSize={24} lineWidth={2} style={{ width: "100% " }}>
-            {[...Array(6)].map((x, i) => (
+            {[...Array(6)].map((_, i) => (
               <Timeline.Item key={`loading-${i}`}>
                 <Flex
                   direction={{ base: "column", xs: "row" }}
@@ -209,33 +199,13 @@ const Exercises = ({ equipment, muscles, workout, setWorkout }) => {
           </Timeline>
         )}
         {!isLoading && !!workout.length && (
-          <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
+          <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="exercises">
               {(provided) => (
-                <div style={{width: '100%'}}>
-                  <Timeline
-                  ref={provided.innerRef} 
-                  {...provided.droppableProps}
-                    bulletSize={24}
-                    lineWidth={2}
-                  >
+                <div style={{ width: "100%" }}>
+                  <div ref={provided.innerRef} {...provided.droppableProps}>
                     {workout.map((exercise, index) => (
-                      <Timeline.Item
-                        key={exercise._id}
-                        sx={{ padding: isDragging ? "0.1rem" : "auto", paddingLeft: "1.5rem !important"}}
-                        bullet={
-                          <Tooltip label={exercise.mainMuscle}>
-                            <ThemeIcon
-                              size={22}
-                              variant="light"
-                              color={muscleToColor[exercise.mainMuscle]}
-                              radius="xl"
-                            >
-                              {exercise.mainMuscle.slice(0, 1)}
-                            </ThemeIcon>
-                          </Tooltip>
-                        }
-                      >
+                      <Flex align="center" key={exercise._id}>
                         <Draggable
                           draggableId={exercise._id}
                           index={index}
@@ -243,91 +213,119 @@ const Exercises = ({ equipment, muscles, workout, setWorkout }) => {
                         >
                           {(draggableProvided, snapshot) => {
                             return (
-                            <Flex
-                              sx={{...draggingStyle(snapshot.isDragging)}}
-                              ref={draggableProvided.innerRef}
-                              direction={{ base: "column", xs: "row" }}
-                              {...draggableProvided.draggableProps}
-                              align={{ base: "flex-start", xs: "center" }}
-                              justify="space-between"
-                            >
-                              
-                              <Flex mr="xl" mb={{ base: "sm", xs: 0 }} align="center" justify="center">
-                                <Flex {...draggableProvided.dragHandleProps} justify="center" mr="xs">
+                              <Flex
+                                sx={{
+                                  flex: 1,
+                                  ...draggingStyle(snapshot.isDragging),
+                                  paddingLeft: 10
+                                }}
+                                align={{ base: "flex-start", xs: "center" }}
+                                direction={{ base: "column", xs: "row" }}
+                                ref={draggableProvided.innerRef}
+                                {...draggableProvided.dragHandleProps}
+                                {...draggableProvided.draggableProps}
+                              >
+                                <Flex
+                                  align="center"
+                                  py={{
+                                    base: 15,
+                                  }}
+                                  justify="space-between"
+                                >
+                                  <Flex justify="center" mr={2}>
                                     <IconArrowsMoveVertical size="1rem" />
+                                  </Flex>
+                                  <Tooltip label={exercise.mainMuscle}>
+                                    <ThemeIcon
+                                        sx={{ cursor: "pointer" }}
+                                      size={22}
+                                      variant="light"
+                                      color={muscleToColor[exercise.mainMuscle]}
+                                      radius="xl"
+                                    >
+                                      {exercise.mainMuscle.slice(0, 1)}
+                                    </ThemeIcon>
+                                  </Tooltip>
+                                  <Flex sx={{cursor: 'default'}} align="center">
+                                  <Text ml={5} mr="xs" fw={500}>
+                                    {exercise.title}
+                                  </Text>
+                                  <VideoIcon url={exercise.videos[0]} />
+                                  </Flex>
                                 </Flex>
-                                <Text mr="xs" fw={500}>
-                                  {exercise.title}
-                                </Text>
-                                <VideoIcon url={exercise.videos[0]} />
-                              </Flex>
 
-                              <Flex>
-                                <Button
-                                  compact
-                                  variant="light"
-                                  color="blue"
-                                  mr="xs"
-                                  onClick={() => shuffleExercise(exercise)}
-                                  leftIcon={<IconArrowsShuffle size="1rem" />}
+                                <Flex justify="flex-end" pb={{
+                                    base: 15,
+                                    sm: 0
+                                }}
+                                    sx={{
+                                        flex: 1,
+                                    }}
                                 >
-                                  Shuffle
-                                </Button>
-                                <Button
-                                  compact
-                                  variant="light"
-                                  color="blue"
-                                  mr="xs"
-                                  onClick={() => selectExercise(exercise)}
-                                  leftIcon={<IconClick size="1rem" />}
-                                >
-                                  Pick
-                                </Button>
-                                <ActionIcon
-                                  variant="outline"
-                                  color="red"
-                                  size="sm"
-                                  onClick={() => removeExercise(exercise)}
-                                >
-                                  <IconTrash size="1rem" />
-                                </ActionIcon>
+                                  <Button
+                                    compact
+                                    variant="light"
+                                    color="blue"
+                                    mr="xs"
+                                    onClick={() => shuffleExercise(exercise)}
+                                    leftIcon={<IconArrowsShuffle size="1rem" />}
+                                  >
+                                    Shuffle
+                                  </Button>
+                                  <Button
+                                    compact
+                                    variant="light"
+                                    color="blue"
+                                    mr="xs"
+                                    onClick={() => selectExercise(exercise)}
+                                    leftIcon={<IconClick size="1rem" />}
+                                  >
+                                    Pick
+                                  </Button>
+                                  <ActionIcon
+                                    variant="outline"
+                                    color="red"
+                                    size="sm"
+                                    onClick={() => removeExercise(exercise)}
+                                  >
+                                    <IconTrash size="1rem" />
+                                  </ActionIcon>
+                                </Flex>
                               </Flex>
-                            </Flex>
-                          )}}
+                            );
+                          }}
                         </Draggable>
-
-                      </Timeline.Item>
+                      </Flex>
                     ))}
-
-                    <Timeline.Item
-                      bullet={
-                        <ThemeIcon
-                          color="blue"
-                          radius="xl"
-                          onClick={addNewExercise}
-                          style={{ cursor: "pointer" }}
-                        >
-                          <IconPlus size="0.8rem" />
-                        </ThemeIcon>
-                      }
-                    >
-                      <Button
-                        compact
-                        variant="light"
-                        color="blue"
-                        mr="xs"
-                        onClick={addNewExercise}
-                      >
-                        Add
-                      </Button>
-                    </Timeline.Item>
-                  </Timeline>
-                  {provided.placeholder}
                   </div>
+                  {provided.placeholder}
+                </div>
               )}
             </Droppable>
           </DragDropContext>
         )}
+
+        <Flex>
+          <Button
+            compact
+            variant="light"
+            color="blue"
+            leftIcon={
+              <ThemeIcon
+                color="blue"
+                radius="xl"
+                size={22}
+                onClick={addNewExercise}
+                style={{ cursor: "pointer" }}
+              >
+                <IconPlus size="0.8rem" />
+              </ThemeIcon>
+            }
+            onClick={addNewExercise}
+          >
+            Add
+          </Button>
+        </Flex>
       </Flex>
       <Modal opened={opened} onClose={close} title="Choose Exercise">
         <SelectModal
@@ -344,10 +342,13 @@ const Exercises = ({ equipment, muscles, workout, setWorkout }) => {
 };
 
 const draggingStyle = (isDragging) => ({
-    background: isDragging ? "#f8f8f8" : "white",
-    border: isDragging ? "1px dashed #ddd" : "none",
-    boxShadow: isDragging ? "0px 4px 8px rgba(0, 0, 0, 0.1)" : "none",
-    transition: "background-color 0.2s ease, border 0.2s ease, box-shadow 0.2s ease",
-    padding: isDragging ? "1rem" : "auto",
-  });
+  background: isDragging ? "#f8f8f8" : "white",
+  border: isDragging ? "1px dashed #ddd" : "none",
+  boxShadow: isDragging ? "0px 4px 8px rgba(0, 0, 0, 0.1)" : "none",
+  transition:
+    "background-color 0.2s ease, border 0.2s ease, box-shadow 0.2s ease",
+  borderLeft: isDragging
+    ? "2px solid rgb(205, 220, 240)"
+    : "2px solid rgba(230, 245, 255, 1)",
+});
 export default Exercises;

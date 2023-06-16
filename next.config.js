beeeca50863e5721d64cc25b4f46e1c4bb936744
Vincenius <vincent.will@vaitrade.de>
2023-06-16 06:57:58 +0200

    init capacitor config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		unoptimized: true
+	},
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
